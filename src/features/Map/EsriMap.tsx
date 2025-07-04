dfa60e7aac91912b5a0bf66683be04f0239c1640
Wanwan Shi <WANWAN.SHI@tetratech.com>
2025-07-04 14:49:28 +0100

    refactor: move GeoJSON layer initialization into useEffect for better lifecycle management

--- src/features/Map/EsriMap.tsx
+++ src/features/Map/EsriMap.tsx
@@ -4,55 +4,54 @@ import MapView from '@arcgis/core/views/MapView';
 import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
 import Legend from '@arcgis/core/widgets/Legend';
 import LayerList from '@arcgis/core/widgets/LayerList';
-
 import Expand from '@arcgis/core/widgets/Expand';
 
 const EsriMap = () => {
   const mapDiv = useRef<HTMLDivElement>(null);
 
-  const layer1 = new GeoJSONLayer({
-    url: 'https://testblobadysh1243.blob.core.windows.net/maplayers/Yorkshire Water Appointed Wastewater Boundaries_2815270447174429793.geojson',
-    outFields: ['*'],
-    visible: true,
-    popupEnabled: true,
-    popupTemplate: {
-      title: 'Yorkshire Water Boundary',
-      content: (feature: {
-        graphic: { attributes: Record<string, unknown> };
-      }) => {
-        const attributes = feature.graphic.attributes;
-        let table = '<table>';
-        for (const key in attributes) {
-          table += `<tr><td><b>${key}</b></td><td>${attributes[key]}</td></tr>`;
-        }
-        table += '</table>';
-        return table;
-      },
-    },
-  });
-
-  const layer2 = new GeoJSONLayer({
-    url: 'https://testblobadysh1243.blob.core.windows.net/maplayers/Northumbrian_Water_Storm_Overflow_Activity_2_view_1158731685660022164.geojson',
-    outFields: ['*'],
-    visible: true,
-    popupEnabled: true,
-    popupTemplate: {
-      title: 'Northumbrian Water Storm Overflow',
-      content: (feature: {
-        graphic: { attributes: Record<string, unknown> };
-      }) => {
-        const attributes = feature.graphic.attributes;
-        let table = '<table>';
-        for (const key in attributes) {
-          table += `<tr><td><b>${key}</b></td><td>${attributes[key]}</td></tr>`;
-        }
-        table += '</table>';
-        return table;
-      },
-    },
-  });
-
   useEffect(() => {
+    const layer1 = new GeoJSONLayer({
+      url: 'https://testblobadysh1243.blob.core.windows.net/maplayers/Yorkshire Water Appointed Wastewater Boundaries_2815270447174429793.geojson',
+      outFields: ['*'],
+      visible: true,
+      popupEnabled: true,
+      popupTemplate: {
+        title: 'Yorkshire Water Boundary',
+        content: (feature: {
+          graphic: { attributes: Record<string, unknown> };
+        }) => {
+          const attributes = feature.graphic.attributes;
+          let table = '<table>';
+          for (const key in attributes) {
+            table += `<tr><td><b>${key}</b></td><td>${attributes[key]}</td></tr>`;
+          }
+          table += '</table>';
+          return table;
+        },
+      },
+    });
+
+    const layer2 = new GeoJSONLayer({
+      url: 'https://testblobadysh1243.blob.core.windows.net/maplayers/Northumbrian_Water_Storm_Overflow_Activity_2_view_1158731685660022164.geojson',
+      outFields: ['*'],
+      visible: true,
+      popupEnabled: true,
+      popupTemplate: {
+        title: 'Northumbrian Water Storm Overflow',
+        content: (feature: {
+          graphic: { attributes: Record<string, unknown> };
+        }) => {
+          const attributes = feature.graphic.attributes;
+          let table = '<table>';
+          for (const key in attributes) {
+            table += `<tr><td><b>${key}</b></td><td>${attributes[key]}</td></tr>`;
+          }
+          table += '</table>';
+          return table;
+        },
+      },
+    });
+
     if (mapDiv.current) {
       const map = new Map({
         basemap: 'topo-vector',
